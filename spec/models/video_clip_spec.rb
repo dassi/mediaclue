1f1b1480dfb706380e4da2f6785e4724b742bbbe
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2009-10-05 15:55:43 +0200

    Commenting out obsolete specs for VideoClip

--- spec/models/video_clip_spec.rb
+++ spec/models/video_clip_spec.rb
@@ -1,13 +1,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe VideoClip do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    VideoClip.create!(@valid_attributes)
-  end
+  # before(:each) do
+  #   @valid_attributes = {
+  #     
+  #   }
+  # end
+  # 
+  # it "should create a new instance given valid attributes" do
+  #   VideoClip.create!(@valid_attributes)
+  # end
 end
