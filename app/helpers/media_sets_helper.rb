0e2ee5500608a7593c5db7c5c9b46400ec44d090
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2009-10-28 15:50:20 +0100

    Minor bugfix in "media per page"-selector

--- app/helpers/media_sets_helper.rb
+++ app/helpers/media_sets_helper.rb
@@ -2,7 +2,7 @@ module MediaSetsHelper
   
   def per_page_select(media_set, per_page, params = [])
     params_string = params.collect{|k,v| "#{k}=#{v}&"}
-    select = select_tag :per_page, options_for_select(['5', '10', '15', '20', '25', '30', '50', '100', ['Alle', 'all']], per_page), 
+    select = select_tag :per_page, options_for_select(['5', '10', '15', '20', '25', '30', '50', '100', ['Alle', 'all']], per_page.to_s), 
                         :onchange => "location='#{media_set_path(media_set)}?#{params_string}per_page='+$('per_page').value"
                       
     "Medien pro Seite: #{select}"
