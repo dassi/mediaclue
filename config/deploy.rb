aeb2485920491644ee34890c5fe7d0161b191b76
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2010-01-22 11:10:41 +0100

    Deployment tasks für DB-Kopierung remote/lokal

--- config/deploy.rb
+++ config/deploy.rb
@@ -135,3 +135,31 @@ namespace :deploy do
     run "touch #{current_path}/tmp/restart.txt"
   end
 end
+
+
+desc 'Dumps the production database to db/production_data.sql on the remote server'
+task :remote_db_dump, :roles => :db, :only => { :primary => true } do
+  run "cd #{current_path} && " +
+    "rake RAILS_ENV=#{rails_env} db:database_dump --trace" 
+end
+
+desc 'Downloads db/production_data.sql from the remote production environment to your local machine'
+task :remote_db_download, :roles => :db, :only => { :primary => true } do  
+  execute_on_servers(options) do |servers|
+    self.sessions[servers.first].sftp.connect do |tsftp|
+      tsftp.get_file "#{deploy_to}/#{current_dir}/db/production_data.sql", "db/production_data.sql" 
+    end
+  end
+end
+
+desc 'Cleans up data dump file'
+task :remote_db_cleanup, :roles => :db, :only => { :primary => true } do  
+  delete "#{current_path}/db/production_data.sql" 
+end 
+
+desc 'Dumps, downloads and then cleans up the production data dump'
+task :remote_db_runner do
+  remote_db_dump
+  remote_db_download
+  remote_db_cleanup
+end
