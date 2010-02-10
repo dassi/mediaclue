dcc3bba3bf85ec0ccfc98560d58116bcc71978fd
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2010-02-10 17:22:10 +0100

    Remove old unused fields from DB

---
+++ db/migrate/20100210155303_remove_obsolete_fields.rb
@@ -0,0 +1,13 @@
+class RemoveObsoleteFields < ActiveRecord::Migration
+  def self.up
+    remove_column :users, :is_group
+    remove_column :users, :ldap_uid
+    remove_column :user_groups, :ldap_uid rescue nil
+  end
+
+  def self.down
+    add_column :users, :is_group, :boolean
+    add_column :users, :ldap_uid, :string
+    add_column :user_groups, :ldap_uid, :string
+  end
+end
