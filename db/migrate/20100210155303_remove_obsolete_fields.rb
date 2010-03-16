f47c606af64de021850d7fed30933ef01390bfff
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2010-03-16 08:46:54 +0100

    Again more agnostic migration

--- db/migrate/20100210155303_remove_obsolete_fields.rb
+++ db/migrate/20100210155303_remove_obsolete_fields.rb
@@ -1,6 +1,6 @@
 class RemoveObsoleteFields < ActiveRecord::Migration
   def self.up
-    remove_column :users, :is_group
+    remove_column :users, :is_group rescue nil
     remove_column :users, :ldap_uid rescue nil
     remove_column :user_groups, :ldap_uid rescue nil
   end
