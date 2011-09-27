8438beb6941d45b82c07fcabd05f55d02438ee4a
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2011-09-27 23:21:57 +0200

    Better sorted groups dropdown list

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -42,7 +42,7 @@ module ApplicationHelper
   end
   
   def options_for_user_group_select
-    groups = UserGroup.all.collect {|g| [g.full_name, g.id]}
+    groups = UserGroup.all_sorted.collect {|g| [g.full_name, g.id]}
 
     groups
   end
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -5,6 +5,8 @@ class UserGroup < ActiveRecord::Base
 
   has_many :user_group_permissions, :dependent => :destroy
 
+  named_scope :all_sorted, :order => 'full_name ASC'
+
   def member?(user)
     self.users.exists?(user.id)
   end
