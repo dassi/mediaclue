a819d191e99d2f776f946fd649b1f3a2afec9d11
Andreas Brodbeck <andreas.brodbeck@mindclue.ch>
2014-12-04 11:28:57 +0100

    Weitere Mime-Typen für neue Office Dokumente und m4v Video

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,12 +1,30 @@
 class Document < Medium
+
+
+  # Mime type of Office documents:
+  # .xlsx   application/vnd.openxmlformats-officedocument.spreadsheetml.sheet
+  # .xltx   application/vnd.openxmlformats-officedocument.spreadsheetml.template
+  # .potx   application/vnd.openxmlformats-officedocument.presentationml.template
+  # .ppsx   application/vnd.openxmlformats-officedocument.presentationml.slideshow
+  # .pptx   application/vnd.openxmlformats-officedocument.presentationml.presentation
+  # .sldx   application/vnd.openxmlformats-officedocument.presentationml.slide
+  # .docx   application/vnd.openxmlformats-officedocument.wordprocessingml.document
+  # .dotx   application/vnd.openxmlformats-officedocument.wordprocessingml.template
+  # .xlam   application/vnd.ms-excel.addin.macroEnabled.12
+  # .xlsb   application/vnd.ms-excel.sheet.binary.macroEnabled.12
+
   CONTENT_TYPES = ['application/pdf',
                    'application/msword',
                    'application/vnd.ms-excel',
                    'application/vnd.ms-powerpoint',
-                   'application/zip',                               
+                   'application/zip',
                    # 'application/x-zip',
                    'application/vnd.oasis.opendocument.text',         # OpenOffice Text-Dokument
                    'application/vnd.oasis.opendocument.spreadsheet',  # OpenOffice Tabellen-Dokument
+                   'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',           # Excel XML
+                   'application/vnd.openxmlformats-officedocument.presentationml.slideshow',      # PowerPoint XML
+                   'application/vnd.openxmlformats-officedocument.presentationml.presentation',   # PowerPoint XML
+                   'application/vnd.openxmlformats-officedocument.wordprocessingml.document',     # Word XML
                    'application/x-shockwave-flash']
   
   def self.type_display_name
